b852d165cf6bbbdb2e0a6f9698d5d1be11993b95
RovinDewan1 <rovin.dewan@stu.ocsb.ca>
2025-05-30 13:17:48 -0400

    Set up sprites, begin dash mechanic
    Imported Character sprites, started coding dash mechanic

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 130
+var SPEED = 130
 const JUMP_VELOCITY = -300.0
+var Dashcounter = 1
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var Dash_timer: Timer = %"Dash-Recharge-Timer"
+
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -12,6 +15,15 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+#timer
+	Dash_timer.start()
+	print("timer start")
+	func on_recharge_timer_timeout():
+		print("timer end")
+#
+
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
@@ -21,6 +33,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
 	#gets direction, -1, 0 , 1
+	
 	
 	#flip sprite 
 	if direction > 0:
@@ -42,4 +55,28 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+#dash setup
+	if Input.is_action_just_pressed("Dash") and Dashcounter > 0:
+		print(Dashcounter)
+		velocity.y += JUMP_VELOCITY
+		velocity.x += direction * (SPEED * 10)
+		Dashcounter = 0
+	
+	#dash recharge timer
+	
+	
+	
+	#-------
+
+
+
+
 	move_and_slide()
+
+
+func on_recharge_timer_ready() -> void:
+	pass # Replace with function body.
+
+
+func on_recharge_timer_timeout() -> void:
+	pass # Replace with function body.
