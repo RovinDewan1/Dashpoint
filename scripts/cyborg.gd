72b19c183e7c087b198323f2354aba7f1f41f527
TheWaffleMan8 <alessio.franco@stu.ocsb.ca>
2025-06-09 12:18:47 -0400

    what the fuck

--- scripts/cyborg.gd
+++ scripts/cyborg.gd
@@ -3,6 +3,8 @@ extends Node2D
 const SPEED = 60
 
 var direction = 1
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
