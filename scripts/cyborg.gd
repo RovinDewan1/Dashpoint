ef3e6bc7a821a19ea23dcfb115e09e8c61baebef
RovinDewan1 <rovin.dewan@stu.ocsb.ca>
2025-06-10 21:17:00 -0400

    Added code to spawn bullets

--- scripts/cyborg.gd
+++ scripts/cyborg.gd
@@ -1,22 +1,17 @@
 extends Node2D
+@export var Bullet : PackedScene = preload("res://Scenes/Bullet.tscn")
+const SPEED = 20
+@onready var bulletorigin: Marker2D = $"bulletorigin"
+@onready var characterlocation: Marker2D = $CharacterBody2D/Characterlocation
 
-const SPEED = 60
 
 var direction = 0
-@onready var ray_cast_right: RayCast2D = $RayCastRight
-@onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var mover: Timer = $mover
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 
-
-func _process(delta):
-	if ray_cast_right.is_colliding():
-		direction = -1
-		animated_sprite.flip_h = true
-	if ray_cast_left.is_colliding():
-		direction = 1
-		animated_sprite.flip_h = false
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	var b = Bullet.instantiate()
+	b.global_position = bulletorigin.global_position
+	owner.add_child(b)
 	
-	position.x += direction * SPEED * delta
