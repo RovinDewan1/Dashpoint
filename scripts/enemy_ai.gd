4d5b286ae4dc6d5742b9273666d996f3c4bb8eb9
RovinDewan1 <rovin.dewan@stu.ocsb.ca>
2025-06-16 13:10:41 -0400

    added failcase.
    Still need to set up winning

--- scripts/enemy_ai.gd
+++ scripts/enemy_ai.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var hittimer: Timer = $Hittimer
 @onready var punch: AudioStreamPlayer2D = $PUNCH
+@onready var character: Node2D = $"../Character"
 
 
 
@@ -11,8 +12,11 @@ func _physics_process(delta: float) -> void:
 	pass
 
 func _on_temp__no_killzone_yet_body_entered(_body: Node2D) -> void:
+	if not _body is CharacterBody2D:
+		return  # Ignore anything that isn't a CharacterBody2D
+	
 	if Dashing.dash == true:
-		print("taketh") #debug
+		#print("taketh") #debug
 		punch.play()
 		Dashing.Dashcounter += 2
 		Dashing.SPEED += 90
@@ -22,7 +26,10 @@ func _on_temp__no_killzone_yet_body_entered(_body: Node2D) -> void:
 		await get_tree().create_timer(0.155).timeout
 		queue_free()
 	else:
+		Gamelogic.gameoverloc = _body.global_position
+		print("Game over triggered at position:", Gamelogic.gameoverloc)
 		Gamelogic.gameover()
+
 
 
 
--- scripts/gamelogic.gd
+++ scripts/gamelogic.gd
@@ -1,5 +1,10 @@
 extends Node
 
+var scene = preload("res://Scenes/Failscreen.tscn").instantiate()
+var gameoverloc = 0
 func gameover():
-	# Engine.time_scale = 0 #THIS WILL STOP GAME, LOOK INTO THIS.
-	pass
+	Engine.time_scale = 0
+	scene.position = gameoverloc
+	scene.z_index = 100  # Set render order
+	add_child(scene)
+	print("fucke")
