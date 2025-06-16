af3a989a957fd0044aff62190e0eccb773ef1166
RovinDewan1 <rovin.dewan@stu.ocsb.ca>
2025-06-16 07:57:02 -0400

    added more enemies, needs debugging.

--- scripts/enemy_ai.gd
+++ scripts/enemy_ai.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-@onready var character_body_2d: CharacterBody2D = $CharacterBody2D
 @onready var hittimer: Timer = $Hittimer
 @onready var punch: AudioStreamPlayer2D = $PUNCH
 
@@ -13,9 +12,13 @@ func _physics_process(delta: float) -> void:
 
 func _on_temp__no_killzone_yet_body_entered(_body: Node2D) -> void:
 	if Dashing.dash == true:
+		print("taketh") #debug
 		punch.play()
 		Dashing.Dashcounter += 2
 		Dashing.SPEED += 90
+		Engine.time_scale = .5
+		await get_tree().create_timer(0.2).timeout
+		Engine.time_scale = 1
 		await get_tree().create_timer(0.155).timeout
 		queue_free()
 	else:
